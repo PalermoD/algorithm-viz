a5d4a2760a0439b851ebeb1c2b90fa187f02b905
PalermoD <palermo.deschamps@gmail.com>
2016-06-24 14:25:16 -0400

    boilerplate for app done, wired webpack and react

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+class App extends Component {
+	render() {
+		return <div>Hello Im the first react componet</div>;
+	}
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));         
