ab05c1584c944ea57700f6309bc5126f50fa14f6
PalermoD <palermo.deschamps@gmail.com>
2016-06-27 15:33:09 -0400

    server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 
 
-app.set('port', (process.env.PORT || 3000));
+
 
 app.use('/', express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
@@ -26,6 +26,3 @@ app.use(function(req, res, next) {
 
 
 
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
-});
